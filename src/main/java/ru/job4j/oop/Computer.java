4f508813d753d314ffcab5d500d3b68e45e113bb
blanksearch <blankinsearch@gmail.com>
2025-07-18 13:05:01 +0200

    [#173890]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -9,22 +9,38 @@ public class Computer {
     public Computer() {
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
 
-    public void printInfo() {
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
+    public void getInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
     }
 
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        Computer comp = new Computer(true, 500, "Intel Core I7-10700K");
-        computer.printInfo();
-        comp.printInfo();
+        Computer first = new Computer();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+
+        first.getInfo();
+        second.getInfo();
+        third.getInfo();
+        forth.getInfo();
     }
 }
