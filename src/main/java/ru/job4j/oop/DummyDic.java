f0651be96a1b3dd3738da29f620c561ed294f3dd
blanksearch <blankinsearch@gmail.com>
2025-07-18 10:25:08 +0200

    [#167387]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+    public static void main(String[] args) {
+    }
+
+    public String engToRus(String eng) {
+        return "Неизвестное слово " + eng;
+    }
+}
