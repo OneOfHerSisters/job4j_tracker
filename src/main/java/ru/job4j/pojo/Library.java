b6505db2be0f7732170f6bc5238b40327f9c45c6
blanksearch <blankinsearch@gmail.com>
2025-07-22 10:51:51 +0200

    [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean code", 400);
+        Book book2 = new Book("War and Peace", 1213);
+        Book book3 = new Book("The Hobbit", 888);
+        Book book4 = new Book("1984", 282);
+        Book[] books = {book1, book2, book3, book4};
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " - " + books[i].getPages() + " pages");
+        }
+
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " - " + books[i].getPages() + " pages");
+        }
+        for (Book book : books) {
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + " - " + book.getPages() + " pages");
+            }
+        }
+    }
+}
