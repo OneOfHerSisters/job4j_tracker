423c6e2c9878ddab6487f960459bd20dc26bcb02
blanksearch <blankinsearch@gmail.com>
2025-07-23 20:35:59 +0200

    [#174892 #548883]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -18,7 +18,7 @@ public class Library {
             System.out.println(books[i].getName() + " - " + books[i].getPages() + " pages");
         }
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getPages() + " pages");
             }
         }
