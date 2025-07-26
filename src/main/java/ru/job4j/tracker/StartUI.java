ebd9f3e3a3b2421b739ff5627284a94a19413753
blanksearch <blankinsearch@gmail.com>
2025-07-26 13:01:22 +0200

    [#500745 #549168]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -20,8 +20,12 @@ public class StartUI {
             } else if (select == 1) {
                 System.out.println("=== Вывод всех заявок ===");
                 Item[] items = tracker.findAll();
-                for (Item item : items) {
-                System.out.println(item);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
                 }
             } else if (select == 2) {
                 System.out.println("=== Редактирование заявки ===");
