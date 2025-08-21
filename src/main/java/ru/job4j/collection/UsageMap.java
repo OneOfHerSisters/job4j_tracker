73606776b8dffa749a347e65747bd7d11d27cb96
blanksearch <blankinsearch@gmail.com>
2025-08-21 10:40:33 +0200

    [#212652 #550642]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> info = new HashMap<>();
         info.put("test@gmail.com", "Jan Kowalski");
+        info.put("test@gmail.com", "Jerzy Trzaskowski");
+        info.put("test2@gmail.com", "Jan Kowalski");
+        info.put("test3@gmail.com", "Magda Boczar");
+        info.put("test3@gmail.com", "Jan Pawel 2");
         for (String key : info.keySet()) {
             System.out.println(key + ": " + info.get(key));
         }
